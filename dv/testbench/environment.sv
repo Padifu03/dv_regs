b407fe16d7e63c80183e468886f83bda631ffac7
JfPadifu_Master_micro <pafuen5@uv.alumni.es>
2024-11-20 13:35:17 +0100

    Added i2c_agent to build phase

--- dv/testbench/environment.sv
+++ dv/testbench/environment.sv
@@ -2,16 +2,17 @@
 
 class environment extends uvm_env;  
   `uvm_component_utils(environment)
-  
+
   i2c_agent i2c_agt;
   //instantiate agents, interface...
-  
+
   function new(string name, uvm_component parent);
     super.new(name,parent);
   endfunction
-	
+
   function void build_phase(uvm_phase phase);
     //create agents
+    i2c_agt = i2c_agent::type_id::create("m_i2c_agt", this);
   endfunction
 
   function void connect_phase(uvm_phase phase);
