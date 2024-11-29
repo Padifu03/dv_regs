be6fb6b18c98a0c7781a0131e05e25846bbbf3cc
JfPadifu_Master_micro <pafuen5@uv.alumni.es>
2024-11-29 20:08:35 +0100

    Added if to base test

--- dv/testbench/base_test.sv
+++ dv/testbench/base_test.sv
@@ -3,7 +3,7 @@
 class base_test extends uvm_test;
 
   environment env;
-
+  virtual dut_if dut_vif;
   `uvm_component_utils(base_test)
 
   function new(string name, uvm_component parent);
@@ -16,33 +16,14 @@ class base_test extends uvm_test;
 
     //create environment
     env = environment::type_id::create("top_env", this);
+
+    //get dut_if
+    if(!uvm_config_db#(virtual dut_if)::get(this, "", "dut_if", dut_vif))
+            `uvm_fatal("NO_VIF",{"virtual interface must be set for:", get_full_name().".vif"} );
   endfunction
 
   //Check build phase
   virtual function void end_of_elaboration_phase (uvm_phase phase);
     uvm_top.print_topology ();
   endfunction
-
-  //Start phase
-  /* No sirve a priori. Si que podemos hacer un par de task para el reset y el clk
-  function void start_of_simulation_phase (uvm_phase phase);
-      super.start_of_simulation_phase (phase);
-
-      // [Optional] Assign a default sequence to be executed by the sequencer or look at the run_phase ...
-      //uvm_config_db#(uvm_object_wrapper)::set(this,"m_top_env.my_agent.m_seqr0.main_phase",
-      //                                        "default_sequence", base_sequence::type_id::get());
-
-   endfunction
-
-  // Run phase
-  // or [Recommended] start a sequence for this particular test
-  virtual task run_phase (uvm_phase phase);
-    my_seq m_seq = my_seq::type_id::create ("m_seq");
-
-    super.run_phase(phase);
-    phase.raise_objection (this);
-    m_seq.start (m_env.seqr);
-    phase.drop_objection (this);
-  endtask
-  */
 endclass
